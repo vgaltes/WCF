66c05d4cab4dc842c639d33167c6a12f8ac470fb
Vicenc Garcia-Altes <vincentredrum@gmail.com>
2018-12-29 11:56:10 +0000

    First build

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -1,4 +1,4 @@
 module.exports.init = () => {
-  process.env.mastersTable = "masters-devmanolete";
+  process.env.mastersTable = `masters-${process.env.TEST_STAGE}`;
   process.env.AWS_REGION = "eu-west-1";
 };
